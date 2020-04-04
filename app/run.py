9eebf6413c42ac4535063d27f344b26257526bbf
Matus Ravas <matustabasco@gmail.com>
2020-04-04 21:57:47 +0200

    rearanged project structure

--- app/run.py
+++ app/run.py
@@ -1,8 +1,8 @@
 from .worker import Worker
-
 worker = Worker()
 
 worker.open_files()
 worker.map_data()
 worker.write_output()
 worker.close_files()
+input("Press Enter to finish...")
--- app/worker.py
+++ app/worker.py
@@ -1,8 +1,6 @@
 import csv
-import datetime as dt
 import time
 import sys
-from calendar import timegm
 from typing import List
 from .dtos import OUTPUT_DATA_DTO, GPS_DTO, SONAR_DTO
 from .utils import get_gps_timestamp, get_sonar_timestamp
@@ -11,16 +9,16 @@ class Worker:
 
     def __init__(self):
         self.data: List[OUTPUT_DATA_DTO] = []
-        self.output_path = './outputs/output.csv'
+        self.output_path = './output.csv'
         self.gps_file = None
         self.sonar_file = None
         self.config_file = None
 
     def open_files(self):
         try:
-            self.gps_file = open('./inputs/gps.csv','r')
-            self.sonar_file = open('./inputs/sonar.csv','r')
-            self.config_file = open('./inputs/config.csv')
+            self.gps_file = open('./gps.csv','r')
+            self.sonar_file = open('./sonar.csv','r')
+            self.config_file = open('./config.csv')
         except FileNotFoundError as err:
             print(err)
             print('Warning, all input files should be .csv')
@@ -58,6 +56,7 @@ class Worker:
 
 
     def map_data(self):
+        begin = time.time()
         gps_data = self.get_gps_csv_dict()
         sonar_data = self.get_sonar_csv_dict()
         loops = 0
@@ -67,30 +66,31 @@ class Worker:
             loops+=1
             for i in range(s, len(sonar_data)):
                 loops+=1
-                sonar_timestamp_next = sonar_data[i+1]['timestamp'] if i+1 < len(sonar_data) else sonar_data[i]['timestamp']  
+                sonar_timestamp = sonar_data[i]['timestamp'] 
+                sonar_timestamp_next = sonar_data[i+1]['timestamp'] if i+1 < len(sonar_data) else sonar_timestamp 
 
-                if abs(gps_row['timestamp'] - sonar_data[i]['timestamp']) < abs(gps_row['timestamp'] - sonar_timestamp_next):
+                if abs(gps_row['timestamp'] - sonar_timestamp) < abs(gps_row['timestamp'] - sonar_timestamp_next):
                     item.depth = sonar_data[i]['depth']
                     item.altitude = sonar_data[i]['altitude']
                     item.datetime = gps_row['datetime']
-                    item.time_diff = round(gps_row['timestamp'] - sonar_data[i]['timestamp'], 2)
+                    item.time_diff = round(gps_row['timestamp'] - sonar_timestamp, 2)
                     s = i
                     self.data.append(item)
                     break
         print(f'Data processed in {loops} loops')
+        print(f'Execution took {time.time() - begin} seconds')
 
 
     def write_output(self):
-        file_ = open(self.output_path,'w',newline='')
-        writer = csv.DictWriter(file_,['title','lat','lon','altitude','depth','datetime','time_diff'], delimiter='\t')
+        output_file = open(self.output_path,'w',newline='')
+        writer = csv.DictWriter(output_file,['title','lat','lon','altitude','depth','datetime','time_diff'], delimiter='\t')
         for row in self.data:
             writer.writerow(row.serialize())
-        file_.close()
+        output_file.close()
+        print(f'Output data written to {self.output_path[2:]}')
 
 
     def close_files(self):
         self.config_file.close()
         self.gps_file.close()
         self.sonar_file.close()
-        print('Finished, open outputs directory to see the output.')
-        input("Press Enter to finish...")
